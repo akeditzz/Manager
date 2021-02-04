f5cae57bb1a7ca19b14c7301e067e7b18586c47c
akeditzz <akshaymanagooli@yahoo.com>
2021-02-04 16:27:12 +0530

    boilerplates and redux,firebase setup done

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
+import firebase from 'firebase'
+
+class App extends Component {
+
+  componentDidMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyAaiHAdF3ClsAjhAqOBGI3j4V2llq0ELfY",
+      authDomain: "manager-86372.firebaseapp.com",
+      projectId: "manager-86372",
+      storageBucket: "manager-86372.appspot.com",
+      messagingSenderId: "33806106837",
+      appId: "1:33806106837:web:d60403c4b598cc6f81225c"
+    })
+  }
+
+  render() {
+    return <Provider store={createStore(reducers)}>
+      <View>
+        <Text>
+          Hello
+        </Text>
+      </View>
+    </Provider>
+  }
+}
+
+export default App
